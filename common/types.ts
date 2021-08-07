f2364eb3bef7c579bd0968bcc1ee20a4a95c24b9
meebs <alex@takayamasan.com>
2021-08-07 18:50:46 -0400

    add types/constants, remove sample code
    - add gitignore
    - create directory `common` for code shared between server and client
    - remove the sample code from the template example

---
+++ common/types.ts
@@ -0,0 +1,20 @@
+/**
+ * @param recipeId optional for now, but in the future this might be required
+ */
+export type Picture = {
+  id: string;
+  author: string;
+  pictureUrl: string;
+  recipeId?: string;
+};
+
+/**
+ * @param photoIds references to photos/creations of this recipe
+ */
+export type Recipe = {
+  id: string;
+  author: string;
+  ingredients: string;
+  steps: string;
+  pictureIds: Array<string>;
+};
