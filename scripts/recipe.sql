96a083a6ee13d932cd4bb396b4812cea76d988f4
aislinblack <1of4black@gmail.com>
2021-08-11 17:28:04 -0400

    Add title column to recipes table

--- scripts/recipe.sql
+++ scripts/recipe.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS recipes;
 CREATE TABLE recipes (
     id SERIAL PRIMARY KEY,
     author varchar(255),
+    title varchar(255),
     ingredients varchar(1024),
     steps varchar(65535), -- longest possible
     image_url varchar(255)
