2e611283116f506b851208bb30cfb29059cdfb66
aislinblack <1of4black@gmail.com>
2021-08-16 16:44:34 -0400

    Update table columns

--- scripts/recipe.sql
+++ scripts/recipe.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS pictures;
 DROP TABLE IF EXISTS recipes;
 
 CREATE TABLE recipes (
@@ -5,6 +6,15 @@ CREATE TABLE recipes (
     author varchar(255),
     title varchar(255),
     ingredients varchar(1024),
-    steps varchar(65535), -- longest possible
-    image_url varchar(255)
+    steps varchar(65535) -- longest possible
+);
+
+CREATE TABLE pictures (
+    id SERIAL PRIMARY KEY,
+    author varchar(255),
+    description varchar(255),
+    title varchar(255),
+    picture_url varchar(255),
+    recipe_id INT,
+    CONSTRAINT fk_recipe FOREIGN KEY(recipe_id) REFERENCES recipes(id) -- picture should respond to one recipe
 );
