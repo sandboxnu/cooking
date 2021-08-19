4ad7919a49412ba61ed45b12daf62301083d6e3d
aislinblack <1of4black@gmail.com>
2021-08-19 13:18:28 -0400

    get recipe id from query

--- pages/recipe/[recipeId].tsx
+++ pages/recipe/[recipeId].tsx
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/dist/client/router';
+
 /**
  * Page To Display A Singular Recipe
  */
-export default function Recipe() {}
+export default function Recipe() {
+  const router = useRouter();
+  const { recipeId } = router.query;
+
+  //TODO: get real recipe from recipeId
+}
